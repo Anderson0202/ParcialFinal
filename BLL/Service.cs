68c2220f7a20a5e89a19afc8967e873367982e73
Anderson0202 <53060401+Anderson0202@users.noreply.github.com>
2020-12-13 12:41:59 -0500

    Creada las capas y conexion a base de datos

---
+++ BLL/Service.cs
@@ -0,0 +1,25 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class Service
+    {
+
+        private ConnectionManager connectionManager;
+        //public ClienteRepository ClienteRepository;
+        //private PedidoRepository PedidoRepository;
+
+        public Service(string connectionString)
+        {
+            connectionManager = new ConnectionManager(connectionString);
+            //ClienteRepository = new ClienteRepository(connectionManager);
+            //PedidoRepository = new PedidoRepository(connectionManager);
+        }
+
+    }
+}
---
+++ DAL/Repository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Repository
+    {
+
+        private ConnectionManager connectionManager { get; }
+
+        private SqlConnection connection;
+
+        public Repository(ConnectionManager _connectionManager)
+        {
+            connectionManager = _connectionManager;
+            connection = connectionManager._conexion;
+        }
+
+
+
+    }
+}
